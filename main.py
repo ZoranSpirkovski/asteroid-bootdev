50bbc18f322dd72511a7648e7e6e057e9879157f
ZoranSpirkovski <forwarding@zorans.anonaddy.com>
2026-02-27 15:49:03 +0100

    Completed chapter 3: player Draw Player lesson

--- main.py
+++ main.py
@@ -2,20 +2,31 @@ import pygame
 from constants import SCREEN_HEIGHT
 from constants import SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids wit pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    gameclock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = gameclock.tick(60)/1000
 
 
 if __name__ == "__main__":
