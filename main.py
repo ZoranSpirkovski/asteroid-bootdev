39997c4ee5edd117146c15d435382fb4b1a901e1
ZoranSpirkovski <forwarding@zorans.anonaddy.com>
2026-02-28 10:23:54 +0100

    completed Chapter 4 lesson 2

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from constants import SCREEN_HEIGHT
 from constants import SCREEN_WIDTH
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from logger import log_event
+import sys
+
 
 def main():
     print(f"Starting Asteroids wit pygame version: {pygame.version.ver}")
@@ -14,7 +19,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+
+    asteroidfield = AsteroidField()
 
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
@@ -30,6 +40,11 @@ def main():
         for item in drawable:
             item.draw(screen)
         updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game Over!")
+                sys.exit()
         pygame.display.flip()
 
 if __name__ == "__main__":
