9810a4c5be40725a243406593a3d3a7cccd92d94
ZoranSpirkovski <forwarding@zorans.anonaddy.com>
2026-02-28 09:36:02 +0100

    completed chapter 3, lesson 5 completely

--- main.py
+++ main.py
@@ -12,11 +12,13 @@ def main():
     gameclock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
-
 
     while True:
         log_state()
@@ -25,11 +27,10 @@ def main():
                 return
         dt = gameclock.tick(60)/1000
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for item in drawable:
+            item.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
-
-
 
 if __name__ == "__main__":
     main()
